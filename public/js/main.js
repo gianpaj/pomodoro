5f6964faac5f2b86fc0f8c9c065ffcfe728c1d8f
Gianfranco Palumbo <gianpa@gmail.com>
2016-05-03 18:33:07 +0100

    Simple timer with moment.js and bootstrap progress bar

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,52 @@
+/* globals moment */
+
 $(document).ready(function() {
-    console.log('jQuery is loaded');
+  console.log('jQuery is loaded');
+
+  var counterFn;
+  var timer;
+  var $progressBar = $('.progress-bar');
+  var $timer = $('#timer');
+
+  $('#start').click(function() {
+    $('.startStop').toggle();
+
+    $progressBar.toggleClass('active');
+
+    // reset progress bar and timer
+    if (!timer) {
+      $progressBar.css('width', 0+'%').attr('aria-valuenow', 0);
+      timer = moment.duration({ minute:0 });
+      $timer.text('00:00');
+    }
+
+    counterFn = setInterval(function () {
+      timer = moment.duration(timer.asSeconds() + 1, 'seconds');
+      $timer.text(moment(timer._data).format('mm:ss'));
+
+      var percentage = timer.asSeconds() * 100 / 1500;
+      $progressBar.css('width', percentage+'%').attr('aria-valuenow', percentage);
+
+      // 25 min = 1500 seconds
+      if (timer.asSeconds() == 1500) {
+        clearInterval(counterFn);
+        console.log('a pomodoro has passed');
+      }
+    }, 1000);
+  });
+
+  $('#stop').click(function() {
+    $('.startStop').toggle();
+    $progressBar.toggleClass('active');
+    clearInterval(counterFn);
+  });
+
+  $('#reset').click(function() {
+    $('#stop').hide();
+    $('#start').show();
+    $progressBar.css('width', 0+'%').attr('aria-valuenow', 0);
+    timer = moment.duration({ minute:0 });
+    $timer.text('00:00');
+    clearInterval(counterFn);
+  });
 });
